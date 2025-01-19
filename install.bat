b864be08d4896dffebe95aad5e7ffd9d100806e0
ajaydutt76 <130857530+ajaydutt76@users.noreply.github.com>
2025-01-19 19:47:13 +0530

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,10 @@
+pip install setuptools
+pip install wheel
+pip install streamlit
+pip install yfinance
+pip install numpy
+pip install pandas
+pip install pandas-ta
+pip install scikit-learn
+pip install tensorflow
+pip install plotly
